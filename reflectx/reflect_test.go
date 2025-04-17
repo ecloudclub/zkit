cb927d242a7430bb451429bebdd007c82f4ab714
crazyfrankie <axu9417@gmail.com>
2025-04-17 21:13:16 +0800

    feat(reflectx):isnil

---
+++ reflectx/reflect_test.go
@@ -0,0 +1,172 @@
+package reflectx
+
+import (
+	"reflect"
+	"testing"
+	"unsafe"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestIsNilValue(t *testing.T) {
+	type MyInterface interface{}
+	// 空接口，用于测试
+	var nilMI MyInterface
+	// 非空接口，用于测试
+	n := 1
+	myInterface := MyInterface(n)
+
+	// nil channel
+	var nilCh chan int
+	// 非 nil channel
+	ch := make(chan int)
+
+	// nil pointer
+	var nilPtr *int
+	// 非 nil pointer
+	ptr := &n
+
+	// nil unsafePointer
+	nilUsPtr := unsafe.Pointer(nilPtr)
+	// 非 nil unsafePointer
+	usPtr := unsafe.Pointer(ptr)
+
+	// nil map
+	var nilMp map[string]struct{}
+	// 非 nil map
+	mp := make(map[string]struct{}, 1)
+
+	// nil slice
+	var nilSlice []int
+	// 非 nil slice
+	slc := make([]int, 1)
+
+	// nil 函数
+	type MyFunc func()
+	var myFunc MyFunc
+
+	testCases := []struct {
+		name string
+		val  reflect.Value
+		res  bool
+	}{
+		{
+			name: "int 类型",
+			val:  reflect.ValueOf(666),
+			res:  false,
+		},
+		{
+			name: "string 类型",
+			val:  reflect.ValueOf("字符串类型"),
+			res:  false,
+		},
+		{
+			name: "bool 类型",
+			val:  reflect.ValueOf(true),
+			res:  false,
+		},
+		{
+			name: "float 类型",
+			val:  reflect.ValueOf(3.14),
+			res:  false,
+		},
+		{
+			name: "complex 类型",
+			val:  reflect.ValueOf(complex(1, 1)),
+			res:  false,
+		},
+		{
+			name: "struct 类型",
+			val:  reflect.ValueOf(struct{}{}),
+			res:  false,
+		},
+		{
+			name: "array 类型",
+			val:  reflect.ValueOf([4]int{}),
+			res:  false,
+		},
+		{
+			name: "nil 非法值",
+			val:  reflect.ValueOf(nil),
+			res:  true,
+		},
+		{
+			name: "interface 类型 - 非空",
+			val:  reflect.ValueOf(myInterface),
+			res:  false,
+		},
+		{
+			name: "interface 类型 - 空",
+			val:  reflect.ValueOf(nilMI),
+			res:  true,
+		},
+		{
+			name: "pointer 类型 - 非空",
+			val:  reflect.ValueOf(ptr),
+			res:  false,
+		},
+		{
+			name: "pointer 类型 - 空",
+			val:  reflect.ValueOf(nilPtr),
+			res:  true,
+		},
+		{
+			name: "unsafePointer 类型 - 非空",
+			val:  reflect.ValueOf(usPtr),
+			res:  false,
+		},
+		{
+			name: "unsafePointer 类型 - 空",
+			val:  reflect.ValueOf(nilUsPtr),
+			res:  true,
+		},
+		{
+			name: "channel 类型 - 非空",
+			val:  reflect.ValueOf(ch),
+			res:  false,
+		},
+		{
+			name: "channel 类型 - 空",
+			val:  reflect.ValueOf(nilCh),
+			res:  true,
+		},
+		{
+			name: "map 类型 - 非空",
+			val:  reflect.ValueOf(mp),
+			res:  false,
+		},
+		{
+			name: "map 类型 - 空",
+			val:  reflect.ValueOf(nilMp),
+			res:  true,
+		},
+		{
+			name: "slice 类型 - 非空",
+			val:  reflect.ValueOf(slc),
+			res:  false,
+		},
+		{
+			name: "slice 类型 - 空",
+			val:  reflect.ValueOf(nilSlice),
+			res:  true,
+		},
+		{
+			name: "func 类型 - 非空",
+			val: reflect.ValueOf(func() func() {
+				return func() {}
+			}),
+			res: false,
+		},
+		{
+			name: "func 类型 - 空",
+			val:  reflect.ValueOf(myFunc),
+			res:  true,
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			res := IsNilValue(tc.val)
+			assert.Equal(t, tc.res, res)
+		})
+	}
+}
