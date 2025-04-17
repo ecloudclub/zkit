e71ffd5b9090090445c605f62fae448e5530175e
crazyfrankie <axu9417@gmail.com>
2025-04-17 16:27:10 +0800

    feat(zapx stringx):sensitive log and unsafe convert

---
+++ zapx/sensitive_log.go
@@ -0,0 +1,31 @@
+package zapx
+
+import "go.uber.org/zap"
+
+type CustomCore struct {
+	zapcore.Core
+}
+
+func NewCustomCore(core zapcore.Core) *CustomCore {
+	return &CustomCore{
+		Core: core,
+	}
+}
+
+func (z *CustomCore) Write(en zapcore.Entry, fields []zapcore.Field) error {
+	for i, fd := range fields {
+		if fd.Key == "phone" {
+			phone := fd.String
+			fields[i].String = phone[:3] + "****" + phone[7:]
+		}
+	}
+
+	return z.Core.Write(en, fields)
+}
+
+func (z *CustomCore) Check(ent zapcore.Entry, ce *zapcore.CheckedEntry) *zapcore.CheckedEntry {
+	if z.Enabled(ent.Level) {
+		return ce.AddCore(ent, z)
+	}
+	return ce
+}
